59a6ddfbe25df3a4d985657976c512842016aca7
Manas Dash <manas.d2@gmail.com>
2024-02-06 19:57:14 +0530

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
 provider "aws" {
   region = var.aws_region
 }
